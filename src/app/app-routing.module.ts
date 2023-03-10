639c49b05c370cc7be608b9740a711034437ccda
HowenXD <hsgonzalezd@itc.edu.co>
2023-03-09 21:18:09 -0500

    Se sube el parcial a git

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RepuestosComponent } from './components/repuestos/repuestos.component';
+import { VehiculosComponent } from './components/vehiculos/vehiculos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'vehiculos' , component:VehiculosComponent
+  },
+  {
+    path: 'repuestos' , component:RepuestosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
